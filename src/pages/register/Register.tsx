30d86a77859407329c3a663d168185c7f5f9f749
mahedihsharif <mahedi5061@gmail.com>
2026-05-07 23:48:40 +0600

    job-tracker-project-register

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,7 +1,18 @@
+import { Briefcase } from "lucide-react";
+
 const Register = () => {
   return (
     <div>
-      <h1>This is Register Page</h1>
+      <div className="h-screen flex items-center justify-center">
+        <div>
+          <div className="flex items-center justify-center w-14 h-14 rounded-xl bg-primary/10">
+            <Briefcase className="w-7 h-7 text-primary" />
+          </div>
+          <h1>Job Tracker</h1>
+          <p>Create an account to get started</p>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
